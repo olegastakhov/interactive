af7bb8e022b58f9bee0588a01c6f8da6425e603d
olegastakhov <132702407+olegastakhov@users.noreply.github.com>
2023-05-05 21:52:43 +0300

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -57,7 +57,7 @@ promoPopupClose.addEventListener("click", (e) => {
 
 // Simulation section
 
-const mousecontainer = document.getElementsByClassName("mousecontainer")[0];
+const mousecontainer = document.getElementsByTagName("section")[0];
 
 const canvas = document.getElementsByTagName("canvas")[0];
 resizeCanvas();
