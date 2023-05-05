f3a53063e84de8a4b53e4aab8b032ce19410d50a
olegastakhov <132702407+olegastakhov@users.noreply.github.com>
2023-05-05 23:51:23 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-const mousefield = document.getElementsById('mousefield')[0];
-const canvas = document.getElementsById('canvas')[0];
+const mousefield = document.getElementById('mousefield')[0];
+const canvas = document.getElementById('canvas')[0];
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
